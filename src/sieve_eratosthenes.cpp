36913e725b6fe3dd682c02fbbb5fd11f01783239
Leviathan <soaresjsantos93@gmail.com>
2024-08-30 00:46:37 -0300

    Feat: some checks and refactors were made.

--- src/sieve_eratosthenes.cpp
+++ src/sieve_eratosthenes.cpp
@@ -3,26 +3,36 @@
 
 std::vector<long long> sieve_eratosthenes(long long &n)
 {
-    std::vector<bool> sieve((n - 1) / 2, true);
+    if (n < 0)
+        throw std::invalid_argument("Error: N is a negative number");
 
-    long long p = 3;
-
-    while (p*p <= n)
+    std::vector<long long> primes;
+    
+    if (n >= 2)
     {
-        if (sieve[(p -1)/ 2 - 1])
+        primes.push_back(2);
+
+        std::vector<bool> sieve((n - 1) / 2, true);
+
+        long long p = 3;
+
+        while (p*p <= n)
         {
-            for (size_t i = p*p; i <= n; i+= 2*p)
-                sieve[(i - 1) / 2 - 1] = false;
+            if (sieve[(p -1)/ 2 - 1])
+            {
+                primes.push_back(p);
+
+                for (size_t i = p*p; i <= n; i+= 2*p)
+                    sieve[(i - 1) / 2 - 1] = false;
+            }
+
+            p += 2;
         }
 
-        p += 2;
+        for (size_t i = (p -1)/ 2 - 1; i < (n - 1) / 2; i++)
+            if (sieve[i])
+                primes.push_back(2*(i + 1) + 1);
     }
-
-    std::vector<long long> primes(1, 2);
-
-    for (size_t i = 0; i < (n - 1) / 2; i++)
-        if (sieve[i])
-            primes.push_back(2*(i + 1) + 1);
 
     return primes;
 }
